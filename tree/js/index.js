cec168d8514021c1e4c59586542645e0b59f71db
SpriteEliot <834556414@qq.com>
2017-03-24 17:38:47 +0800

    增加定时器功能，动态绘制图线，目前测试中

--- tree/js/index.js
+++ tree/js/index.js
@@ -1,44 +1,52 @@
+var num = 1;
 var line = document.getElementById('line');
-var state=true;
+var state = true;
 class drawTree {
-    constructor(line, drawLine, head) {
+    constructor(line, drawLine, head, timer, i, length) {
         this.line = document.getElementById('line');
         this.drawLine = this.line.getContext('2d');
         this.head = document.getElementById('head');
+        this.drawLine.strokeStyle = '#2e6da4';
+        this.timer = timer;
+        this.line.width = 1000;
+        this.line.height = 500;
+        this.length = 100;
     }
 
 
     drawTeam(x, y) {
-        this.drawLine.strokeStyle = '#ff0000';
-        this.drawLine.moveTo(x, y);
-        this.drawLine.lineTo(0, y * 2);
-        this.drawLine.stroke();
-        this.drawLine.moveTo(x, y);
-        this.drawLine.lineTo(x / 2, y * 2);
-        this.drawLine.stroke();
-        this.drawLine.moveTo(x, y);
-        this.drawLine.lineTo(x, y * 2);
-        this.drawLine.stroke();
-        this.drawLine.moveTo(x, y);
-        this.drawLine.lineTo(x * 2, y * 2);
-        this.drawLine.stroke();
-        this.drawLine.moveTo(x, y);
-        this.drawLine.lineTo(x * 1.5, y * 2);
-        this.drawLine.stroke();
+        var _self = this;
+
+        function start() {
+
+            if (num < _self.length) {
+                _self.drawLine.moveTo(x, y);
+                _self.drawLine.lineTo(++x, ++y);
+                num++;
+                _self.drawLine.stroke();
+                console.log(x);
+            } else {}
+
+        };
+        setInterval(function () {
+            start();
+        }, 5);
     }
 
     drawHead(x, y) {
-        this.drawLine.drawImage(this.head, x - 30, y - 140);
+        this.drawLine.drawImage(this.head, x, y);
     }
 
     drawAll(x, y) {
-        this.drawClear();
-        this.drawTeam(x, y);
-        this.drawHead(x, y);
+        var _self = this;
+        this.drawTeam(x, y)
+        //clearInterval(setInterval('this.drawTeam(x,y)',1000));
+        //setInterval('_self.drawTeam(100,100)',1000);
+        //this.drawHead(x, y);
     }
-    drawClear(){
-        this.line.width = 1000;
-        this.line.height = 500;
+    drawClear() {
+        this.line.width = 1440;
+        this.line.height = 900;
         this.drawLine.clearRect(0, 0, 1000, 500);
 
     }
@@ -46,15 +54,23 @@ class drawTree {
 
 function draw() {
     var tree = new drawTree();
-    tree.drawAll(500, 250)
+    tree.drawAll(500, 200);
+
 }
+
 function clear() {
     var tree = new drawTree();
     tree.drawClear();
 }
 
 function drawSwitch() {
-    if(state){draw();state=false;}else if(state=0){clear();state=true;}
+    if (state) {
+        let t = setTimeout(draw(), 3000);
+        state = false;
+    } else if (state = 0) {
+        clear();
+        state = true;
+    }
 }
 
 /*
@@ -67,3 +83,4 @@ function getMousePos(event) {
     tree.drawAll(x, y);
 }
 */
+;
